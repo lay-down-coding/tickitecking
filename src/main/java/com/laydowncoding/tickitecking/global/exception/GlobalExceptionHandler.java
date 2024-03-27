030f949831299f330c7f6cd37cdcae7906fd9bde
Hu6r1s <darmaa333@gmail.com>
2024-03-27 21:52:05 +0900

    feat(Global): Global 공통 규약 설정

---
+++ src/main/java/com/laydowncoding/tickitecking/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.laydowncoding.tickitecking.global.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j(topic = "GlobalExceptionHandler -> ")
+public class GlobalExceptionHandler {
+
+}
