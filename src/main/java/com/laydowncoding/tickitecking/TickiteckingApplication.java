8ef7358ca44824ba90a1b7a7fd1519cba2f4dea8
Hu6r1s <darmaa333@gmail.com>
2024-03-27 20:32:06 +0900

    init commit

---
+++ src/main/java/com/laydowncoding/tickitecking/TickiteckingApplication.java
@@ -0,0 +1,13 @@
+package com.laydowncoding.tickitecking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickiteckingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TickiteckingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/laydowncoding/tickitecking/TickiteckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laydowncoding.tickitecking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickiteckingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
