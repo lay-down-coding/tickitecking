afe0b91fff23d3b3d668eb7936e015a427e6e95c
jinkshower <askthem@naver.com>
2024-03-28 14:24:01 +0900

    test: SignupRequestDto 생성 테스트

---
+++ src/test/java/com/laydowncoding/tickitecking/domain/user/dto/SignupRequestDtoTest.java
@@ -0,0 +1,57 @@
+package com.laydowncoding.tickitecking.domain.user.dto;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SignupRequestDtoTest {
+
+    @DisplayName("생성 성공")
+    @Test
+    void create_success() {
+        //given
+        SignupRequestDto requestDto = SignupRequestDto.builder()
+            .username("test_username")
+            .password("pass@i344")
+            .email("test@email.com")
+            .nickname("johny")
+            .build();
+
+        //when
+        Set<ConstraintViolation<SignupRequestDto>> validate = validate(requestDto);
+
+        //then
+        assertThat(validate).isEmpty();
+    }
+
+    @DisplayName("생성 실패")
+    @Test
+    void create_fail() {
+        //given
+        SignupRequestDto requestDto = SignupRequestDto.builder()
+            .username("")
+            .password("fail")
+            .email("test")
+            .nickname("johny")
+            .build();
+
+        //when
+        Set<ConstraintViolation<SignupRequestDto>> validate = validate(requestDto);
+
+        //then
+        assertThat(validate).hasSize(3);
+    }
+
+    private Set<ConstraintViolation<SignupRequestDto>> validate(SignupRequestDto requestDto) {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        return validator.validate(requestDto);
+    }
+}
