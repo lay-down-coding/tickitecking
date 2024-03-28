9dbbcd2f7441640a2f00178d7d323360302f1dd5
Hu6r1s <darmaa333@gmail.com>
2024-03-28 14:05:46 +0900

    test(Auditorium): 공연장 등록 API service 테스트 구현

---
+++ src/test/java/com/laydowncoding/tickitecking/domain/auditorium/repository/AuditoriumRepository.java
@@ -0,0 +1,17 @@
+package com.laydowncoding.tickitecking.domain.auditorium.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class AuditoriumRepository {
+
+  @Autowired
+  private AuditoriumRepository auditoriumRepository;
+
+
+}
---
+++ src/test/java/com/laydowncoding/tickitecking/domain/auditorium/service/AuditoriumServiceTest.java
@@ -0,0 +1,43 @@
+package com.laydowncoding.tickitecking.domain.auditorium.service;
+
+import static org.mockito.Mockito.*;
+
+import com.laydowncoding.tickitecking.domain.auditorium.dto.request.AuditoriumRequestDto;
+import com.laydowncoding.tickitecking.domain.auditorium.entity.Auditorium;
+import com.laydowncoding.tickitecking.domain.auditorium.repository.AuditoriumRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuditoriumServiceTest {
+
+  @Mock
+  private AuditoriumRepository auditoriumRepository;
+
+  @InjectMocks
+  private AuditoriumServiceImpl auditoriumService;
+
+  @Test
+  public void create_auditorium() {
+    AuditoriumRequestDto requestDto = new AuditoriumRequestDto(
+        "auditorium1",
+        "address1",
+        "C",
+        "30"
+    );
+
+    when(auditoriumRepository.save(any(Auditorium.class))).thenReturn(new Auditorium());
+
+    auditoriumService.createAuditorium(
+        requestDto.getName(),
+        requestDto.getAddress(),
+        requestDto.getMaxColumn(),
+        requestDto.getMaxRow()
+    );
+
+    verify(auditoriumRepository, times(1)).save(any(Auditorium.class));
+  }
+}
