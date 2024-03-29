be365482224ece5f53a3e1041f0d0ec4f3d3aef6
jinkshower <askthem@naver.com>
2024-03-29 19:25:12 +0900

    refactor: eventcontroller import오류 제거

--- src/main/java/com/laydowncoding/tickitecking/domain/event/controller/EventController.java
+++ src/main/java/com/laydowncoding/tickitecking/domain/event/controller/EventController.java
@@ -1,6 +1,5 @@
 package com.laydowncoding.tickitecking.domain.event.controller;
 
-import com.laydowncoding.tickitecking.domain.auditoriums.service.AuditoriumService;
 import com.laydowncoding.tickitecking.domain.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
