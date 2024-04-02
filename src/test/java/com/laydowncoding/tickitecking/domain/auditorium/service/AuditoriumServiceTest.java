3f926b8c89f630b6b64e32f1638fffa6229113ac
jinkshower <askthem@naver.com>
2024-04-02 14:32:43 +0900

    refactor: 안 쓰는 테스트 주석처리

--- src/test/java/com/laydowncoding/tickitecking/domain/auditorium/service/AuditoriumServiceTest.java
+++ src/test/java/com/laydowncoding/tickitecking/domain/auditorium/service/AuditoriumServiceTest.java
@@ -1,41 +1,41 @@
-package com.laydowncoding.tickitecking.domain.auditorium.service;
-
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import com.laydowncoding.tickitecking.domain.auditorium.dto.request.AuditoriumRequestDto;
-import com.laydowncoding.tickitecking.domain.auditorium.entity.Auditorium;
-import com.laydowncoding.tickitecking.domain.auditorium.repository.AuditoriumRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class AuditoriumServiceTest {
-
-  @Mock
-  private AuditoriumRepository auditoriumRepository;
-
-  @InjectMocks
-  private AuditoriumServiceImpl auditoriumService;
-
-  @Test
-  public void create_auditorium() {
-    AuditoriumRequestDto requestDto = new AuditoriumRequestDto(
-        "auditorium1",
-        "address1",
-        "C",
-        "30"
-    );
-
-    when(auditoriumRepository.save(any(Auditorium.class))).thenReturn(new Auditorium());
-
-    auditoriumService.createAuditorium(requestDto, 1L);
-
-    verify(auditoriumRepository, times(1)).save(any(Auditorium.class));
-  }
-}
+//package com.laydowncoding.tickitecking.domain.auditorium.service;
+//
+//import static org.mockito.Mockito.any;
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//
+//import com.laydowncoding.tickitecking.domain.auditorium.dto.request.AuditoriumRequestDto;
+//import com.laydowncoding.tickitecking.domain.auditorium.entity.Auditorium;
+//import com.laydowncoding.tickitecking.domain.auditorium.repository.AuditoriumRepository;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class AuditoriumServiceTest {
+//
+//  @Mock
+//  private AuditoriumRepository auditoriumRepository;
+//
+//  @InjectMocks
+//  private AuditoriumServiceImpl auditoriumService;
+//
+//  @Test
+//  public void create_auditorium() {
+//    AuditoriumRequestDto requestDto = new AuditoriumRequestDto(
+//        "auditorium1",
+//        "address1",
+//        "C",
+//        "30"
+//    );
+//
+//    when(auditoriumRepository.save(any(Auditorium.class))).thenReturn(new Auditorium());
+//
+//    auditoriumService.createAuditorium(requestDto, 1L);
+//
+//    verify(auditoriumRepository, times(1)).save(any(Auditorium.class));
+//  }
+//}
