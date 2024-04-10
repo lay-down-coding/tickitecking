09b2a746613529ede08368494c1ba855bf3a921c
Hu6r1s <darmaa333@gmail.com>
2024-04-10 18:29:24 +0900

    refactor(Test): @SpringBootTest 어노테이션에 class 지정

--- src/test/java/com/laydowncoding/tickitecking/TickiteckingApplicationTests.java
+++ src/test/java/com/laydowncoding/tickitecking/TickiteckingApplicationTests.java
@@ -3,7 +3,7 @@ package com.laydowncoding.tickitecking;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TickiteckingApplicationTests.class)
 class TickiteckingApplicationTests {
 
   @Test
