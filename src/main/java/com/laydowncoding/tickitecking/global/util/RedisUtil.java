323d7b6b85496bca7444d54ec3530ec207d36fa3
jinkshower <askthem@naver.com>
2024-04-11 16:35:44 +0900

    refactor: lua script stringbuffer로 교체

--- src/main/java/com/laydowncoding/tickitecking/global/util/RedisUtil.java
+++ src/main/java/com/laydowncoding/tickitecking/global/util/RedisUtil.java
@@ -14,16 +14,18 @@ public class RedisUtil {
     private final RedisTemplate<String, Object> redisTemplate;
 
     public String addSet(String key, String value, Long expiredTime) {
-        String script = "local keyExists = redis.call('exists', KEYS[1]) " +
-            "local isAdded " +
-            "if keyExists == 0 then " +
-            "    redis.call('sadd', KEYS[1], ARGV[1]) " +
-            "    redis.call('expire', KEYS[1], ARGV[2]) " +
-            "    isAdded = 1 " +
-            "else " +
-            "    isAdded = redis.call('sadd', KEYS[1], ARGV[1]) " +
-            "end " +
-            "return tostring(isAdded)";
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("local keyExists = redis.call('exists', KEYS[1]) ");
+        stringBuffer.append("local isAdded ");
+        stringBuffer.append("if keyExists == 0 then ");
+        stringBuffer.append("    redis.call('sadd', KEYS[1], ARGV[1]) ");
+        stringBuffer.append("    redis.call('expire', KEYS[1], ARGV[2]) ");
+        stringBuffer.append("    isAdded = 1 ");
+        stringBuffer.append("else ");
+        stringBuffer.append("    isAdded = redis.call('sadd', KEYS[1], ARGV[1]) ");
+        stringBuffer.append("end ");
+        stringBuffer.append("return tostring(isAdded)");
+        String script = stringBuffer.toString();
         DefaultRedisScript<String> luaScript = new DefaultRedisScript<>(script, String.class);
         List<String> keys = Collections.singletonList(key);
 
