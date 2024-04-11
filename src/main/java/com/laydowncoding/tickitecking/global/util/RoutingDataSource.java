db60c8244df4c57b431ec1254b83394a00f97b46
Hu6r1s <darmaa333@gmail.com>
2024-04-11 18:54:19 +0900

    bugs(CQRS): @Nullable 어노테이션 제거

--- src/main/java/com/laydowncoding/tickitecking/global/util/RoutingDataSource.java
+++ src/main/java/com/laydowncoding/tickitecking/global/util/RoutingDataSource.java
@@ -2,12 +2,10 @@ package com.laydowncoding.tickitecking.global.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.lang.Nullable;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
-  @Nullable
   @Override
   protected Object determineCurrentLookupKey() {
     String lookupKey = TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "replica" : "source";
