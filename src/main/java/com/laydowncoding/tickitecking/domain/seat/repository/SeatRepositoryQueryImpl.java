557fef9f29a76e1777da6de41b946bceaec4546a
jinkshower <askthem@naver.com>
2024-04-18 17:50:58 +0900

    style: 안쓰는 import 제거

--- src/main/java/com/laydowncoding/tickitecking/domain/seat/repository/SeatRepositoryQueryImpl.java
+++ src/main/java/com/laydowncoding/tickitecking/domain/seat/repository/SeatRepositoryQueryImpl.java
@@ -1,12 +1,7 @@
 package com.laydowncoding.tickitecking.domain.seat.repository;
 
-import static com.laydowncoding.tickitecking.domain.seat.entity.QSeat.seat;
-
-import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class SeatRepositoryQueryImpl implements SeatRepositoryQuery {
-
 }
