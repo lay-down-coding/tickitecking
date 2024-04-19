3379b5876f6312ade7c7a80e3fe2b359bf8544e1
Hu6r1s <darmaa333@gmail.com>
2024-04-19 19:37:24 +0900

    refactor(Concert): count 쿼리 최적화

--- src/main/java/com/laydowncoding/tickitecking/domain/concert/repository/ConcertRepositoryImpl.java
+++ src/main/java/com/laydowncoding/tickitecking/domain/concert/repository/ConcertRepositoryImpl.java
@@ -6,7 +6,6 @@ import static com.laydowncoding.tickitecking.domain.image.entity.QImage.image;
 import static com.laydowncoding.tickitecking.domain.user.entity.QUser.user;
 
 import com.laydowncoding.tickitecking.domain.concert.dto.AllConcertResponseDto;
-import com.laydowncoding.tickitecking.domain.concert.entitiy.Concert;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -14,43 +13,43 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ConcertRepositoryImpl extends QuerydslRepositorySupport implements
-    ConcertRepositoryQuery {
+public class ConcertRepositoryImpl implements ConcertRepositoryQuery {
 
   private final JPAQueryFactory queryFactory;
 
   public ConcertRepositoryImpl(JPAQueryFactory queryFactory) {
-    super(Concert.class);
     this.queryFactory = queryFactory;
   }
 
 
   @Override
   public Page<AllConcertResponseDto> getAllConcerts(Pageable pageable) {
-    JPQLQuery<AllConcertResponseDto> query = getQuerydsl().applyPagination(pageable,
-        queryFactory.select(
-                Projections.constructor(
-                    AllConcertResponseDto.class,
-                    concert.id,
-                    concert.name,
-                    image.filePath,
-                    concert.startTime,
-                    user.nickname,
-                    auditorium.name
-                )
+    JPQLQuery<AllConcertResponseDto> query = queryFactory.select(
+            Projections.constructor(
+                AllConcertResponseDto.class,
+                concert.id,
+                concert.name,
+                image.filePath,
+                concert.startTime,
+                user.nickname,
+                auditorium.name
             )
-            .from(concert)
-            .join(user).on(concert.companyUserId.eq(user.id))
-            .join(auditorium).on(concert.auditoriumId.eq(auditorium.id))
-            .join(image).on(concert.id.eq(image.concertId))
-            .orderBy(concert.id.desc()));
+        )
+        .from(concert)
+        .join(user).on(concert.companyUserId.eq(user.id))
+        .join(auditorium).on(concert.auditoriumId.eq(auditorium.id))
+        .join(image).on(concert.id.eq(image.concertId))
+        .orderBy(concert.id.desc());
 
-    List<AllConcertResponseDto> responses = query.fetch();
-    Long totalCount = query.fetchCount();
-    return new PageImpl<>(responses, pageable, totalCount);
+    List<AllConcertResponseDto> data = query
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    return PageableExecutionUtils.getPage(data, pageable, query::fetchCount);
   }
 }
